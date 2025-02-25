710345e17359368dd0fbbbff6118ceb3a529a5af
slaclau <77557628+slaclau@users.noreply.github.com>
2025-02-25 18:58:29 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs and removes/replaces all required packages
-packages.sh
+/ctx/packages.sh
 
 # Use a COPR Example:
 #
