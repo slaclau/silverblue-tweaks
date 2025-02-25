2e69d4233aa24628f3690f95ff0305107a3d3486
Sebastien Laclau <seb.laclau@gmail.com>
2025-02-25 20:36:54 +0000

    Add emblem-warning.png for plymouth

--- customization.sh
+++ customization.sh
@@ -7,3 +7,5 @@ cd /tmp
 wget https://github.com/slaclau/yaru-extra-icons/archive/master.tar.gz
 tar xf master.tar.gz
 cp -r yaru-extra-icons-master/Custom /usr/share/icons/
+
+cp /ctx/data/emblem-warning.png /usr/share/plymouth/themes/spinner/
