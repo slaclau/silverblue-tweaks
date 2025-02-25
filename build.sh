268bb16fbbb95bd5d510d4d68c57c5c6b3ee80e5
slaclau <77557628+slaclau@users.noreply.github.com>
2025-02-25 18:53:54 +0000

    Update build.sh
    Add reference to package.sh

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf install -y tmux 
+# this installs and removes/replaces all required packages
+packages.sh
 
 # Use a COPR Example:
 #
