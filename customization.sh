5a02ce7189fd62544648c3ea735e7a60b1834928
slaclau <77557628+slaclau@users.noreply.github.com>
2025-02-25 20:07:12 +0000

    Update customization.sh

--- customization.sh
+++ customization.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+set -ouex pipefail
+
 cd /tmp
 
 wget https://github.com/slaclau/yaru-extra-icons/archive/master.tar.gz
 tar xf master.tar.gz
-cp yaru-extra-icons-master/Custom /usr/share/icons/
+cp -r yaru-extra-icons-master/Custom /usr/share/icons/
