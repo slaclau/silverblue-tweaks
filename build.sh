0c2a22cf5d25da1e572683259cecd4554330538c
Sebastien Laclau <seb.laclau@gmail.com>
2025-03-01 00:32:49 +0000

    Add groups to build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,18 @@ set ${SET_X:+-x} -eou pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs and removes/replaces all required packages
+echo "::group::Install packages"
 /ctx/packages.sh
+echo "::endgroup::"
+
+echo "::group::Install packages"
 /ctx/customization.sh
+echo "::endgroup::"
+
+echo "::group::Install packages"
 /ctx/initramfs.sh
+echo "::endgroup::"
+
+echo "::group::Install packages"
 /ctx/cleanup.sh
+echo "::endgroup::"
