93eb669e1c51bca5ed31a36985fdf63b6207cf0d
Sebastien Laclau <seb.laclau@gmail.com>
2025-03-01 17:28:13 +0000

    Fix

--- build.sh
+++ build.sh
@@ -9,6 +9,10 @@ set ${SET_X:+-x} -eou pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs and removes/replaces all required packages
+echo "::group::Install kernel and akmods as needed"
+/ctx/kernel.sh
+echo "::endgroup::"
+
 echo "::group::Install packages"
 /ctx/packages.sh
 echo "::endgroup::"
