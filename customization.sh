0a95b1b462986b95374eff5771f0d46e8e9e79a9
Sebastien Laclau <seb.laclau@gmail.com>
2025-03-04 18:20:03 +0000

    Update /etc/os-release

--- customization.sh
+++ customization.sh
@@ -9,3 +9,38 @@ tar xf master.tar.gz
 cp -r yaru-extra-icons-master/Custom /usr/share/icons/
 
 cp /ctx/data/emblem-warning.png /usr/share/plymouth/themes/spinner/
+
+IMAGE_PRETTY_NAME="Silverblue Tweaks"
+IMAGE_LIKE=fedora
+
+HOME_URL="https://github.com/slaclau/silverblue-tweaks"
+DOCUMENTATION_URL="https://github.com/slaclau/silverblue-tweaks"
+SUPPORT_URL="https://github.com/slaclau/silverblue-tweaks/issues/"
+BUG_SUPPORT_URL="https://github.com/slaclau/silverblue-tweaks/issues/"
+
+CODE_NAME=""
+
+sed -i "s|^VARIANT_ID=.*|VARIANT_ID=$IMAGE_NAME|" /usr/lib/os-release
+sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"${IMAGE_PRETTY_NAME} (Version: ${VERSION} / FROM Fedora ${BASE_IMAGE_NAME^} $FEDORA_MAJOR_VERSION)\"|" /usr/lib/os-release
+sed -i "s|^NAME=.*|NAME=\"$IMAGE_PRETTY_NAME\"|" /usr/lib/os-release
+sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
+sed -i "s|^DOCUMENTATION_URL=.*|DOCUMENTATION_URL=\"$DOCUMENTATION_URL\"|" /usr/lib/os-release
+sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
+sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"$BUG_SUPPORT_URL\"|" /usr/lib/os-release
+#sed -i "s|^CPE_NAME=\"cpe:/o:fedoraproject:fedora|CPE_NAME=\"cpe:/o:universal-blue:${IMAGE_PRETTY_NAME,}|" /usr/lib/os-release
+sed -i "s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME=\"${IMAGE_NAME}\"|" /usr/lib/os-release
+sed -i "s|^ID=fedora|ID=${IMAGE_NAME}\nID_LIKE=\"${IMAGE_LIKE}\"|" /usr/lib/os-release
+sed -i "/^REDHAT_BUGZILLA_PRODUCT=/d; /^REDHAT_BUGZILLA_PRODUCT_VERSION=/d; /^REDHAT_SUPPORT_PRODUCT=/d; /^REDHAT_SUPPORT_PRODUCT_VERSION=/d" /usr/lib/os-release
+sed -i "s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"$CODE_NAME\"|" /usr/lib/os-release
+sed -i "s|^VERSION=.*|VERSION=\"${VERSION} (${BASE_IMAGE_NAME^})\"|" /usr/lib/os-release
+sed -i "s|^OSTREE_VERSION=.*|OSTREE_VERSION=\'${VERSION}\'|" /usr/lib/os-release
+sed -i "s|^RELEASE_TYPE=.*|RELEASE_TYPE=\"$STREAM\"|" /usr/lib/os-release
+
+if [[ -n "${SHA_HEAD_SHORT:-}" ]]; then
+  echo "BUILD_ID=\"$SHA_HEAD_SHORT\"" >>/usr/lib/os-release
+fi
+
+# Added in systemd 249.
+# https://www.freedesktop.org/software/systemd/man/latest/os-release.html#IMAGE_ID=
+echo "IMAGE_ID=\"${IMAGE_NAME}\"" >> /usr/lib/os-release
+echo "IMAGE_VERSION=\"${VERSION}\"" >> /usr/lib/os-release
