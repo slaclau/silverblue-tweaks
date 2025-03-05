701d458e8c5f39d8b2b67f85af386f432e30f043
Sebastien Laclau <seb.laclau@gmail.com>
2025-03-05 14:11:29 +0000

    Add kernel gating

--- kernel.sh
+++ kernel.sh
@@ -7,5 +7,11 @@ wanted=$KERNEL
 if [[ $installed == $wanted ]]; then
   echo "Nothing to do"
 else
-  echo "TBC: change kernel - $installed to $wanted"
+  echo "Change kernel - $installed to $wanted"
+  kernel_packages=$(rpm -qa 'kernel*')
+  for pkg in $kernel_packages; do
+    rpm --erase $pkg --nodeps
+  done
+  kernel_packages=$(echo $kernel_packages | sed "s/${installed}/${wanted}/g")
+  dnf install -y $kernel_packages
 fi
