045d4aa0a018c2551da26f66ddca5ead4930986c
Sebastien Laclau <seb.laclau@gmail.com>
2025-03-05 14:37:23 +0000

    Fix

--- kernel.sh
+++ kernel.sh
@@ -13,5 +13,5 @@ else
     rpm --erase $pkg --nodeps
   done
   kernel_packages=$(echo $kernel_packages | sed "s/${installed}/${wanted}/g")
-  yum install -y $kernel_packages
+  rpm-ostree install -y $kernel_packages
 fi
