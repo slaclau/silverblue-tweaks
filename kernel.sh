7a89726779af7caadf960b93132330db8dc76d9b
slaclau <77557628+slaclau@users.noreply.github.com>
2026-07-04 19:56:33 +0100

    Add Nvidia RPM installation and configuration steps

--- kernel.sh
+++ kernel.sh
@@ -15,3 +15,32 @@ else
   kernel_packages=$(echo $kernel_packages | sed "s/${installed}/${wanted}/g")
   rpm-ostree install -y $kernel_packages
 fi
+
+if [[ "${IMAGE_NAME}" =~ nvidia ]]; then
+    # Fetch Nvidia RPMs
+    skopeo copy --retry-times 3 docker://ghcr.io/ublue-os/akmods-nvidia-open:"${AKMODS_FLAVOR}"-"$(rpm -E %fedora)"-"${KERNEL}" dir:/tmp/akmods-rpms
+    NVIDIA_TARGZ=$(jq -r '.layers[].digest' </tmp/akmods-rpms/manifest.json | cut -d : -f 2)
+    tar -xvzf /tmp/akmods-rpms/"$NVIDIA_TARGZ" -C /tmp/
+    mv /tmp/rpms/* /tmp/akmods-rpms/
+
+    # Exclude the Golang Nvidia Container Toolkit in Fedora Repo
+    # Exclude for non-beta.... doesn't appear to exist for F42 yet?
+    if [[ "${UBLUE_IMAGE_TAG}" != "beta" ]]; then
+        dnf5 config-manager setopt excludepkgs=golang-github-nvidia-container-toolkit
+    else
+        # Monkey patch right now...
+        if ! grep -q negativo17 <(rpm -qi mesa-dri-drivers); then
+            dnf5 -y swap --repo=updates-testing \
+                mesa-dri-drivers mesa-dri-drivers
+        fi
+    fi
+
+    # Install Nvidia RPMs
+    IMAGE_NAME="${BASE_IMAGE_NAME}" AKMODNV_PATH="/tmp/akmods-rpms" MULTILIB=0 /tmp/akmods-rpms/ublue-os/nvidia-install.sh
+    rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
+    ln -sf libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
+    tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<EOF
+kargs = ["rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1", "initcall_blacklist=simpledrm_platform_driver_init"]
+EOF
+fi
+
